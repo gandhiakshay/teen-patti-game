e344a5d72e536c3c5b25759447a5925e388a90d8
Akshay Gandhi <gandhiakshay02@gmail.com>
2017-01-25 11:07:37 +0530

    Created Game class for who is winner

---
+++ src/main/java/com/teen/patti/Game.java
@@ -0,0 +1,27 @@
+package com.teen.patti;
+
+public class Game {
+
+	private String winnerName;
+	private int priority;
+	private String cards;
+	
+	public Game(String winnerName, int priority, String cards) {
+		super();
+		this.winnerName = winnerName;
+		this.priority = priority;
+		this.cards = cards;
+	}
+
+	public String getWinnerName() {
+		return winnerName;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public String getCards() {
+		return cards;
+	}
+}
